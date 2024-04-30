352e5e6ce7f5b94fdccd16afad098ce16bb0130b
Athul2530 <162152975+Athul2530@users.noreply.github.com>
2024-04-30 21:00:18 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0"> 
+<title>index page</title>
+ <link rel="stylesheet" href="style.css">
+
+</head>
+<body style="  background-image: url('busimages/kstrct.jpg');background-size:100% 100%;"></style>
+  <div class="side-bar">
+    <h1 style="text-align:center;">Yathra</h1>
+    <div class="list">
+        <div class="list1"><a href="">Link 1</a></div>
+        <div class="list1"><a href="">Link 1</a></div>
+        <div class="list1"><a href="">Link 1</a></div>
+        <div class="list1"><a href="">Link 1</a></div>
+        
+</div>
+  </div>
+  <div class="content">
+  <div class="header">abc</div>
+  <div class="box">
+    <div class='container'></div>
+    <div class='container'></div>
+    <div class='container'></div>
+</div>
+</div>
+ 
+
+  </body>
+  </html>   
+      
