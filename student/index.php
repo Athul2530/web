2e91f75402d7dfc6381171b041ce34004193a592
Athul2530 <162152975+Athul2530@users.noreply.github.com>
2024-07-21 14:38:15 +0530

    index.php

---
+++ student/index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>index page</title>
+    <link rel="stylesheet" href="style2.css">
+
+</head>
+
+<body style="  background-image: url('busimages/kstrct.jpg');background-size:100% 100%;">
+
+    <?php include('link.php');?>
+
+
+    <div class="content">
+        <h1 style="color:blue; margin-top:100px;margin-left:120px;">Apply For Bus Pass</h1>
+        <button type="button" class="indexb" style="margin-left:200px;"><a href="Studentpass.php">Click
+                HERE</a></button>
+
+    </div>
+
+
+</body>
+
+</html>
