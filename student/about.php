82601818e6c89772e66e22acd1348822aedf41b7
Athul2530 <162152975+Athul2530@users.noreply.github.com>
2024-07-21 14:35:39 +0530

    about.php

---
+++ student/about.php
@@ -0,0 +1,39 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>index page</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+
+<body style="background-color:purple;">
+    <?php include('link.php');?>
+    <div class="about">
+        <div class="pc1">
+
+            <h6 class="section" style="font-size:20px;color:Red">About Us</h6>
+            <h1 class="mb-4" style="text-align:center;">Welcome to YATHRA</h1>
+            <p class="mb-4" style="color:white;">Yathra is a vital asset for educational institutions seeking to
+                optimize transportation
+                services for their students. By offering a seamless and efficient process for issuing and managing
+                student bus passes, Yathra streamlines administrative tasks and reduces the burden on educational staff.
+                This system not only simplifies the pass issuance process but also enhances overall management,
+                providing administrators with tools to track pass usage, monitor validity, and ensure compliance with
+                transportation policies. Through Yathra, educational institutions can prioritize student satisfaction by
+                providing hassle-free access to transportation, fostering a conducive learning environment. This
+                innovative solution reflects our commitment to supporting educational institutions in delivering quality
+                services and enhancing the student experience.</p>
+            <p style="color:yellow;"><i style="color:yellow;"></i>Contact us: 9626728278</p>
+            <p style="color:orange;"><i class="fas fa-map-marker-alt location-icon"></i>Kasargod , Kerala</p>
+        </div>
+        <div class="pc2">
+            <div class="case1"></div>
+            <div class="case2"></div>
+            <div class="case3"></div>
+            <div class="case4"></div>
+        </div>
+    </div>
+</body>
+
+</html>
