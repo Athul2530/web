39e4aee60135d1948bc210edadd075134757173d
Athul2530 <162152975+Athul2530@users.noreply.github.com>
2024-07-21 13:52:05 +0530

    nav.php

---
+++ admin/nav.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0"> 
+<title>index page</title>
+<link rel="stylesheet" href="style2.css">
+</head>
+<body>
+<div class="adminbar">
+    <div class="nav-links">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="contact.php">Contact</a>
+      <a href="Studentpass.php">Student pass</a>
+          <a href="login.php"style="color:yellow;background-color:rgb(238, 130, 238);font-size:30px;font-style:poppins;">Login</a>
+          <a href="register.php"style="color:yellow;background-color:red;font-size:30px;">register</a>
+          
+    </div>
+</div>
+</body>
+</html>
